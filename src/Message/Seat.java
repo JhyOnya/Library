47c6aa3e577c332fb6bb65f9cdc0d2e0baf8cbd1
JhyOnya <785631614@qq.com>
2019-04-24 17:18:54 +0800

    更新界面，已经开始能看了

--- src/Message/Seat.java
+++ src/Message/Seat.java
@@ -7,8 +7,9 @@ import java.util.Map;
 import java.util.TreeMap;
 
 public class Seat {
-	// 教室，座位从1开始；教师数量目前设为5
-	public static int rooms = 5;// 教室数量
+	// 教室和座位从0开始，显示从1开始，UI方进行更正即可；教室数量目前设为5；座位数量目前设为5
+	public static int roomNum = 5;// 教室数量
+	public static int seatNum = 5;// 座位数量
 	public static Map<Integer, Map<Integer, String>> vip;// <1,<3,lih>>,教室1座位3是lih的vip座位
 	public static Map<Integer, List<Integer>> seats;// <1,[2,3]>，教室1中,座位2和3被占用
 
@@ -36,19 +37,34 @@ public class Seat {
 	}
 
 	// 是否是VIP。参数：教室+座位号+姓名
-	public static Map<Integer, Integer> isVIP(String name) {
-		Map<Integer, Integer> seat = new TreeMap<Integer, Integer>();
-		if (false) {
-			// 是vip？我先默认不是vip。是vip的话调用selectSeat，即直接选择vip的默认座位
-			seat.put(1, 1);
-			selectSeat(1, 1);
-		} else
-			seat.put(0, 0);
+	public static int[] isVIP(String name) {
+		int[] seat = new int[2];
+		if (name.equals("vip")) {
+			// 是vip？是vip的话返回教室+座位
+			seat[0] = 1;
+			seat[1] = 1;
+		} else {
+			seat[0] = -1;
+			seat[1] = -1;
+		}
 		return seat;
+	}
+
+	// 获取当前房间座位情况，在list中的是不可使用的（vip+有人）
+	public static List<Integer> getroomSeat(int room) {
+		return seats.get(room);
+	}
+
+	public static String getSeatTime(String name) {
+		return ("10:20:34");
 	}
 
 	// 测试用
 	public static void main(String[] args) {
+		init();
+	}
+	// 测试用
+	public static void init() {
 		selectSeat(1, 2);
 		selectSeat(1, 3);
 		selectSeat(2, 3);
--- src/frameOperate/mainFrame.java
+++ src/frameOperate/mainFrame.java
@@ -18,15 +18,32 @@ import javax.swing.JComboBox;
 import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import java.awt.event.ActionListener;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Map;
 import java.awt.event.ActionEvent;
 import java.awt.SystemColor;
 import java.awt.Color;
+import java.awt.FlowLayout;
+import java.awt.event.FocusAdapter;
+import java.awt.event.FocusEvent;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseMotionAdapter;
+import java.awt.event.InputMethodListener;
+import java.awt.event.InputMethodEvent;
+import java.beans.PropertyChangeListener;
+import java.beans.PropertyChangeEvent;
+import java.beans.VetoableChangeListener;
+import java.awt.event.ItemListener;
+import java.awt.event.ItemEvent;
 
 public class mainFrame extends JFrame {
 
-	static String imgDir = "E:\\Code\\Java\\Library\\src\\Message\\picUI\\";
+	private int x, y;
 	String[] rooms;
+	static String imgDir = "E:\\Code\\Java\\Library\\src\\Message\\picUI\\";
+	private List<JButton> seatBts;
 
 	static mainFrame mframe;
 	private JPanel contentPane;
@@ -35,12 +52,19 @@ public class mainFrame extends JFrame {
 	private JComboBox chooseClass;
 	private JTextField textField_1;
 	private JTextField nameText;
+	private JButton btnNewButton;
+	private JButton buttonG;
+	private JButton buttonY;
+	private JButton buttonR;
+	JButton loginBt;
+	private JTextField textPlease;
 
 	public static void main(String[] args) {
 		EventQueue.invokeLater(new Runnable() {
 			public void run() {
 				try {
 					mframe = new mainFrame();
+					mframe.setUndecorated(true);// 去除边框
 					mframe.setVisible(true);
 				} catch (Exception e) {
 					e.printStackTrace();
@@ -50,8 +74,8 @@ public class mainFrame extends JFrame {
 	}
 
 	public void init() {
-		rooms = new String[Seat.rooms];
-		for (int i = 0; i < Seat.rooms; i++) {
+		rooms = new String[Seat.roomNum];
+		for (int i = 0; i < Seat.roomNum; i++) {
 			rooms[i] = (i + 1) + "";
 		}
 	}
@@ -60,6 +84,7 @@ public class mainFrame extends JFrame {
 	 * Create the frame.
 	 */
 	public mainFrame() {
+		Seat.init();
 		init();
 		this.setSize(677, 459);
 		Dimension screensize = Toolkit.getDefaultToolkit().getScreenSize();
@@ -69,62 +94,211 @@ public class mainFrame extends JFrame {
 		this.setLocation(fx, fy);
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		contentPane = new JPanel();
-		contentPane.setBackground(new Color(216, 191, 216));
+		contentPane.setBackground(new Color(204, 204, 255));
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		setContentPane(contentPane);
 		contentPane.setLayout(null);
 
 		chooseClass = new JComboBox(rooms);
+		chooseClass.addItemListener(new ItemListener() {
+			public void itemStateChanged(ItemEvent e) {
+				if (e.getStateChange() == ItemEvent.SELECTED) {
+					refreshRoom(Integer.parseInt(chooseClass.getSelectedItem() + "") - 1);
+				}
+			}
+		});
 		chooseClass.setEnabled(false);
-		chooseClass.setBounds(598, 10, 41, 21);
+		chooseClass.setBounds(598, 37, 41, 21);
 		contentPane.add(chooseClass);
 
 		nameText = new JTextField();
-		nameText.setBounds(237, 10, 163, 21);
+		nameText.addFocusListener(new FocusAdapter() {
+			@Override
+			public void focusGained(FocusEvent e) {
+				if (nameText.getText().equals("请输入姓名进行登录！"))
+					nameText.setText("");
+			}
+		});
+		nameText.setBounds(237, 37, 163, 21);
 		contentPane.add(nameText);
 		nameText.setColumns(10);
 
 		preTime = new JTextField();
-		preTime.setBackground(new Color(216, 191, 216));
+		preTime.setBackground(new Color(204, 204, 255));
 		preTime.setEditable(false);
 		preTime.setColumns(10);
 		preTime.setBorder(null);
-		preTime.setBounds(10, 10, 170, 21);
+		preTime.setBounds(10, 37, 170, 21);
 		contentPane.add(preTime);
 
 		textField = new JTextField();
-		textField.setBackground(new Color(216, 191, 216));
+		textField.setBackground(new Color(204, 204, 255));
 		textField.setText("\u59D3\u540D\uFF1A");
 		textField.setEditable(false);
 		textField.setColumns(10);
 		textField.setBorder(null);
-		textField.setBounds(197, 10, 66, 21);
+		textField.setBounds(197, 37, 66, 21);
 		contentPane.add(textField);
 
 		textField_1 = new JTextField();
-		textField_1.setBackground(new Color(216, 191, 216));
+		textField_1.setBackground(new Color(204, 204, 255));
 		textField_1.setText("\u9009\u62E9\u6559\u5BA4\uFF1A");
 		textField_1.setEditable(false);
 		textField_1.setColumns(10);
 		textField_1.setBorder(null);
-		textField_1.setBounds(541, 10, 66, 21);
+		textField_1.setBounds(541, 37, 66, 21);
 		contentPane.add(textField_1);
 
-		JButton loginBt = new JButton();
+		loginBt = new JButton();
 		loginBt.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
-				Map<Integer, Integer> stuMsg = Seat.isVIP(nameText.getName());
+				int[] stuMsg;
+				if (nameText.getText().isEmpty() || nameText.getText().equals("请输入姓名进行登录！"))
+					nameText.setText("请输入姓名进行登录！");
+				else {
+					stuMsg = Seat.isVIP(nameText.getText());
+					if (stuMsg[0] == -1) {
+						login();
+					} else {
+						// vip，指定座位
+						Seat.selectSeat(stuMsg[0], stuMsg[1]);
+					}
+				}
 			}
 		});
-		loginBt.setBounds(410, 10, 86, 23);
+		loginBt.setBounds(410, 37, 86, 23);
 		ImageIcon iconStart = new ImageIcon(imgDir + "buttonLogin.png");
 		iconStart.setImage(iconStart.getImage().getScaledInstance(loginBt.getBounds().width, loginBt.getBounds().height,
 				Image.SCALE_SMOOTH));
+		loginBt.setIcon(iconStart);
 		loginBt.setContentAreaFilled(false);
 		loginBt.setBorderPainted(false);
-		loginBt.setIcon(iconStart);
 		contentPane.add(loginBt);
+
+		JPanel panel = new JPanel();
+		panel.setBackground(new Color(204, 204, 255));
+		panel.setBounds(10, 89, 641, 321);
+		contentPane.add(panel);
+		panel.setLayout(new FlowLayout(FlowLayout.CENTER, 5, 5));
+
+		btnNewButton = new JButton("New button");
+		btnNewButton.setEnabled(false);
+		panel.add(btnNewButton);
+
+		JButton buttonG = new JButton();
+		buttonG.setBounds(getWidth() - 63, 0, 21, 21);
+		ImageIcon iconG = new ImageIcon(imgDir + "buttonG.png");
+		iconG.setImage(iconG.getImage().getScaledInstance(buttonG.getBounds().width, buttonG.getBounds().height,
+				Image.SCALE_SMOOTH));
+		buttonG.setIcon(iconG);
+		contentPane.add(buttonG);
+
+		JButton buttonY = new JButton();
+		buttonY.setBounds(getWidth() - 42, 0, 21, 21);
+		ImageIcon iconY = new ImageIcon(imgDir + "buttonY.png");
+		iconY.setImage(iconY.getImage().getScaledInstance(buttonY.getBounds().width, buttonY.getBounds().height,
+				Image.SCALE_SMOOTH));
+		buttonY.setIcon(iconY);
+		buttonY.addMouseListener(new MouseAdapter() {
+			public void mousePressed(MouseEvent e) {
+				x = e.getX();
+				y = e.getY();
+			}
+		});
+		buttonY.addMouseMotionListener(new MouseMotionAdapter() {
+			public void mouseDragged(MouseEvent e) {
+				int left = mframe.getLocation().x;
+				int top = mframe.getLocation().y;
+				mframe.setLocation(left + e.getX() - x, top + e.getY() - y);
+			}
+		});
+		contentPane.add(buttonY);
+
+		JButton buttonR = new JButton();
+		buttonR.setBounds(getWidth() - 21, 0, 21, 21);
+		ImageIcon iconR = new ImageIcon(imgDir + "buttonR.png");
+		iconR.setImage(iconR.getImage().getScaledInstance(buttonR.getBounds().width, buttonR.getBounds().height,
+				Image.SCALE_SMOOTH));
+		buttonR.setIcon(iconR);
+		buttonR.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+				System.exit(0);
+			}
+		});
+		contentPane.add(buttonR);
+
+		textPlease = new JTextField();
+		textPlease.setText("\u8BF7\u9009\u62E9\u5EA7\u4F4D\uFF01");
+		textPlease.setEditable(false);
+		textPlease.setColumns(10);
+		textPlease.setBorder(null);
+		textPlease.setBackground(new Color(204, 204, 255));
+		textPlease.setBounds(291, 68, 76, 21);
+		textPlease.setVisible(false);
+		contentPane.add(textPlease);
+
+		seatBts = new ArrayList<JButton>();
+		for (int i = 0; i < Seat.seatNum; i++) {
+			seatBts.add(new JButton(i + 1 + ""));
+			seatBts.get(i).addActionListener(new ActionListener() {
+				public void actionPerformed(ActionEvent e) {
+					chooseSeat(Integer.parseInt(chooseClass.getSelectedItem() + "") - 1,
+							Integer.parseInt(((JButton) e.getSource()).getText()) - 1);
+				}
+			});
+			seatBts.get(i).setEnabled(false);
+			panel.add(seatBts.get(i));
+		}
 		new Thread(new preTime()).start();
+	}
+
+	public void login() {
+		loginBt.setBounds(410, 37, 86, 23);
+		ImageIcon iconStart = new ImageIcon(imgDir + "buttonLog.png");
+		iconStart.setImage(iconStart.getImage().getScaledInstance(loginBt.getBounds().width, loginBt.getBounds().height,
+				Image.SCALE_SMOOTH));
+		loginBt.setIcon(iconStart);
+		loginBt.setContentAreaFilled(false);
+		loginBt.setEnabled(false);
+		nameText.setEnabled(false);
+		textPlease.setVisible(true);
+		chooseClass.setEnabled(true);
+		for (int i = 0; i < Seat.seatNum; i++) {
+			seatBts.get(i).setEnabled(true);
+		}
+	}
+
+	public void chooseSeat(int room, int seat) {
+		System.out.println(room);
+		System.out.println(seat);
+		logout();
+	}
+
+	public void refreshRoom(int room) {
+		for (int i = 0; i < Seat.seatNum; i++) {
+			seatBts.get(i).setEnabled(true);
+		}
+		List<Integer> preSeats = Seat.getroomSeat(room);
+		for (int seat : preSeats) {
+			seatBts.get(seat).setEnabled(true);
+		}
+	}
+
+	public void logout() {
+		chooseClass.setSelectedIndex(0);
+		loginBt.setBounds(410, 37, 86, 23);
+		ImageIcon iconStart = new ImageIcon(imgDir + "buttonLogin.png");
+		iconStart.setImage(iconStart.getImage().getScaledInstance(loginBt.getBounds().width, loginBt.getBounds().height,
+				Image.SCALE_SMOOTH));
+		loginBt.setIcon(iconStart);
+		loginBt.setContentAreaFilled(true);
+		loginBt.setEnabled(true);
+		nameText.setEnabled(true);
+		textPlease.setVisible(false);
+		chooseClass.setEnabled(false);
+		for (int i = 0; i < Seat.seatNum; i++) {
+			seatBts.get(i).setEnabled(false);
+		}
 	}
 
 	public static void refreshTime(String str) {
