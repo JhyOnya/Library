b2a711ffc2bb30b18bdcb9933505c3a9c61bf228
ChengYM <2063439269@qq.com>
2019-04-27 12:46:24 +0800

    Update getTime.java

--- src/timeOperate/getTime.java
+++ src/timeOperate/getTime.java
@@ -4,76 +4,141 @@ import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
 import java.util.Date;
-import java.util.GregorianCalendar;
+import java.util.Map;
+import java.util.TreeMap;
 
 public class getTime {
-	String baseDate = "2019-04-15";	//表示基准时间，第一周第一天
-	public int weekNum=1;//表示第几周
-	public int dayOfWeek=1;//1表示周一
-	public String remainTime=null;//表示剩余的时间
-	/**
-	 * null表示今天一天都没有课，座位被占据
-	 * */
+	private static String baseDate = "2019-04-15";	
+	private static int weekNum=1;
+	private static int dayOfWeek=1;
+	private static String remainTime=null;
+	private static String[] timeList=new String[10];
+	private static Map<String,Map<Integer,String[]>> schedule;
+
 	
 	
 	
-	//计算本周是第几周&&星期几
-	public void calWeekAndDay() throws ParseException {
+	public static void calWeekAndDay() throws ParseException {
 		Calendar cal = Calendar.getInstance();
 		SimpleDateFormat ft = new SimpleDateFormat("yyyy-MM-dd");
-		Date date1 = new Date();//表示今天的日期
+		Date date1 = new Date();
 		String sdate1 = ft.format(date1);
 		date1 = ft.parse(sdate1);
-		//对时间进行规整
 		Date date2 = ft.parse(baseDate);
 		long days = (date1.getTime()-date2.getTime()) / (24*3600*1000);
-		//因为返回的是毫秒级别
-		System.out.println("days:" + days);
-		//System.out.println(date1.getTime());
-		//将相隔的天数除以7，向上取整就是第几周
+//		System.out.println("days:" + days);
 		long intervalDays=days+1;
-		System.out.println("intervalDays:"+intervalDays);
+//		System.out.println("intervalDays:"+intervalDays);
 		weekNum=(int) Math.ceil(intervalDays/7.0);
-		System.out.println("weekNum:"+weekNum);
+//		System.out.println("weekNum:"+weekNum);
 		int TrueDayOfWeek=cal.get(Calendar.DAY_OF_WEEK);
-		System.out.println("day:"+TrueDayOfWeek);
+//		System.out.println("day:"+TrueDayOfWeek);
 		//注意了4表示星期三，3表示星期二。
-		//验证自己计算的日期是不是和程序得出来得一样
 		int countDayOfWeek=(int)(intervalDays%7);
-		System.out.println("countDayOfWeek:"+countDayOfWeek);
+//		System.out.println("countDayOfWeek:"+countDayOfWeek);
 		if(countDayOfWeek!=TrueDayOfWeek-1)
-			System.out.println("这个计算时间的系统怕是有问题");
+			System.out.println("时间计算错误！");
 		dayOfWeek=TrueDayOfWeek-1;//表示周几
 	}
 	
 	
 
-	//计算剩余时间
-	/**
-	 * 参数
-	 * 1、班级：
-	 * 2、第几周
-	 * 3、星期几
-	 * 首先查看是不是在有课的周数范围里
-	 * 然后确定时间
-	 * */
-	public void calRemainTime(String className) {//还有两个参数不需要传递可以直接用
+	// 计算剩余时间
+	public static void calRemainTime(String className) throws ParseException {
+		calWeekAndDay();
+		String[] time=schedule.get(className).get(dayOfWeek);
 		/**
-		 * TODO:
-		 * 读表格，根据weekNum和班级确定本周有没有课，若null说明这个座位一直显示有人
-		 * 若有课，根据（周数&班级&周几）确定今天的课程时间阈值，计算此时到下一个终止数值之间的时间差
-		 * 时刻更新所有vip的座位情况
-		 * 返回：String "小时:分:秒"，剩余可使用时间
+		 * 处理课表数据
 		 * */
+		
+		
+		
+	}
+//	public static void convertTime(String[] time) {//将当天的时间列表转换成float类型
+//		float[] fTime=new float[10];
+//		for(int i=0;i<time.length;i++) {
+//			String[] a=time[i].split(":");
+////			System.out.println(time[2]);
+//			if(a.length!=1){
+//				fTime[i]=Float.parseFloat(a[0]+"."+a[1]);
+//				System.out.println(fTime[i]);
+//			}
+//			
+//			
+//		}
+//	}
+//	
+	public static void timeList(Date curTime) throws ParseException {
+		// args:表示当时的时间，时-分-秒
+		//和列表进行比较
+		SimpleDateFormat ft = new SimpleDateFormat("HH:mm:ss");
+		if(timeList[0]!=null&&curTime.before(ft.parse(timeList[0]))) {
+			System.out.println("占用图书馆");
+		}
+		else if(timeList[9]!=null&&ft.parse(timeList[0]).before(curTime)) {
+			System.out.println("占用图书馆");
+		}
+		else {
+			
+			for(int i=1;i<timeList.length;i++) {
+				if(timeList[i]!=null) {
+					Date time2=ft.parse(timeList[i]);
+					if(curTime.before(time2)) {//找到了
+						System.out.println("找到了");
+						if(i%2==1) {//正在上课
+							System.out.println(i);
+							long diff=time2.getTime()-curTime.getTime();
+							long hour=(diff/(60*60*1000));
+							long min=((diff-hour*60*60*1000)/(60*1000));
+							long sec=((diff-hour*60*60*1000-min*60*1000)/1000);
+							remainTime=hour+":"+min+":"+sec;
+							System.out.println("距离下课还有:"+remainTime);
+						}
+						else {
+							System.out.println("占用图书馆！");
+							remainTime=null;
+						}
+						return;
+					}
+				}
+				
+			}
+			System.out.println("没有找到");
+			remainTime=null;
+			return;
+		}
+		
+		
+		
+	}
+	
+	
+	public static void init() {//初始化上课时间
+		for(int i=2;i<timeList.length;i++) {
+			timeList[i]=null;
+		}
+		timeList[0]="08:00:00";
+		timeList[1]="09:40:00";
+		timeList[6]="15:30:00";
+		timeList[7]="17:10:00";
+		
 	}
 	
 	
 	
-	
+	//测试
 	public static void main(String[] args) throws ParseException {
-//		Date t=new Date();
-//		SimpleDateFormat df=new SimpleDateFormat("yyyy-MM-dd hh:mm:ss");
-//		System.out.println(df.format(t));
+		init();
+		SimpleDateFormat ft = new SimpleDateFormat("HH:mm:ss");
+		Date date1 = new Date();
+//		String sdate1 = ft.format(date1);
+		String sdate1="16:00:00";
+		Date curTime = ft.parse(sdate1);
+		
+		//转换成字符串
+//		System.out.println("1".compareTo("2"));
+//		System.out.println("2".compareTo("1"));
+		timeList(curTime);
 		
 	
 	}
